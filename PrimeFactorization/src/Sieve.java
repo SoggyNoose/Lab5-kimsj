e3a0470f825170e1ae4bda389f7f0108f32db936
Sam Kim <kimsj@rose-hulman.edu>
2012-03-26 10:11:37 -0400

    Added code to pass testFOur

--- PrimeFactorization/src/Sieve.java
+++ PrimeFactorization/src/Sieve.java
@@ -6,10 +6,25 @@ public class Sieve {
 	public static ArrayList<Integer> generatePrimes(int n){
 		ArrayList<Integer> ret = new ArrayList<Integer>();
 		
-		if (n>2) {
-			ret.add(2);
+		ArrayList<Boolean> sieve = new ArrayList<Boolean>();
+		for(int i=0; i<n; i++) {
+			sieve.add(false);
 		}
 		
+		for(int i=2; i<n/2; i++) {
+			if (!sieve.get(i)) {
+				for(int j=2*i; j<n; j += i) {
+					sieve.set(j, true);
+				}
+			}
+		}
+		
+		for(int i=2; i < n; i++) {
+			if (!sieve.get(i)) {
+				ret.add(i);
+			}
+		}
+			
 		return ret;
 	}
 }
