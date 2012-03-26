c9a635299110e780f3d762b3111b479fa10d2e33
Sam Kim <kimsj@rose-hulman.edu>
2012-03-26 09:49:58 -0400

    Added code to pass testNine

--- PrimeFactorization/src/PrimeFactorization.java
+++ PrimeFactorization/src/PrimeFactorization.java
@@ -6,19 +6,16 @@ public class PrimeFactorization {
 	public static ArrayList<Integer> generatePrimes(int n){
 		ArrayList<Integer> ret = new ArrayList<Integer>();
 		
-		
+		int candidate = 2;
 		while (n>1) {
-			if ((n%2)==0) {
-				ret.add(2);
-				n /= 2;
+			if ((n%candidate)==0) {
+				ret.add(candidate);
+				n /= candidate;
 			} else {
-				break;
+				candidate++;
 			}
 		}
-		if (n > 1) {
-			ret.add(n);
-		}
-		
+				
 		return ret;
 	}
 }
