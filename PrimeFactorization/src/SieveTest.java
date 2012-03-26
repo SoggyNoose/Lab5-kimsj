77e3abc96541618184977aedf2a2a28c95af8479
Sam Kim <kimsj@rose-hulman.edu>
2012-03-26 09:54:09 -0400

    Added testOne

--- PrimeFactorization/src/SieveTest.java
+++ PrimeFactorization/src/SieveTest.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+
 
 
 public class SieveTest {
 
+	@Test
+	public void testOne() {
+		assertEquals(list(), Sieve.generatePrimes(1));
+	}
+	
+	private ArrayList<Integer> list(int... ints){
+		ArrayList<Integer> ret = new ArrayList<Integer>();
+		for(int i : ints){
+			ret.add(i);
+		}
+		return ret;
+	}
 }
