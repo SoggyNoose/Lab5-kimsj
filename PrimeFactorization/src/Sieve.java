8c2ceee7208de4d4ab1ecc5ae0437ff3ac3ed7c2
Sam Kim <kimsj@rose-hulman.edu>
2012-03-26 10:15:16 -0400

    Slight reforctoring, added comments

--- PrimeFactorization/src/Sieve.java
+++ PrimeFactorization/src/Sieve.java
@@ -5,13 +5,15 @@ public class Sieve {
 
 	public static ArrayList<Integer> generatePrimes(int n){
 		ArrayList<Integer> ret = new ArrayList<Integer>();
-		
 		ArrayList<Boolean> sieve = new ArrayList<Boolean>();
+		
+		// Initialize array
 		for(int i=0; i<n; i++) {
 			sieve.add(false);
 		}
 		
-		for(int i=2; i<n/2; i++) {
+		// Mark off candidates
+		for(int i=2; i<Math.sqrt(n); i++) {
 			if (!sieve.get(i)) {
 				for(int j=2*i; j<n; j += i) {
 					sieve.set(j, true);
@@ -19,6 +21,7 @@ public class Sieve {
 			}
 		}
 		
+		// Find all remaining candidates
 		for(int i=2; i < n; i++) {
 			if (!sieve.get(i)) {
 				ret.add(i);
