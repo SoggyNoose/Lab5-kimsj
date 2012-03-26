661fafa70fec5b8b4d1df37a74fbf90ab5a6950e
Sam Kim <kimsj@rose-hulman.edu>
2012-03-26 09:59:13 -0400

    Added code to pass testThree

--- PrimeFactorization/src/Sieve.java
+++ PrimeFactorization/src/Sieve.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 public class Sieve {
 
 	public static ArrayList<Integer> generatePrimes(int n){
-		return new ArrayList<Integer>();
+		ArrayList<Integer> ret = new ArrayList<Integer>();
+		
+		if (n>2) {
+			ret.add(2);
+		}
+		
+		return ret;
 	}
 }
