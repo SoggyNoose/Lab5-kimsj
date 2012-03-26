ea802e89996f9d11470680291a0276e2d95f516c
Sam Kim <kimsj@rose-hulman.edu>
2012-03-26 09:44:25 -0400

    Added code to pass testFour

--- PrimeFactorization/src/PrimeFactorization.java
+++ PrimeFactorization/src/PrimeFactorization.java
@@ -6,6 +6,15 @@ public class PrimeFactorization {
 	public static ArrayList<Integer> generatePrimes(int n){
 		ArrayList<Integer> ret = new ArrayList<Integer>();
 		
+		
+		while (n>1) {
+			if ((n%2)==0) {
+				ret.add(2);
+				n /= 2;
+			} else {
+				break;
+			}
+		}
 		if (n > 1) {
 			ret.add(n);
 		}
