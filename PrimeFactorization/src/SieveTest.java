52409d38a652df004a9f97168412fc3d40dab674
Sam Kim <kimsj@rose-hulman.edu>
2012-03-26 10:00:17 -0400

    Added testFour

--- PrimeFactorization/src/SieveTest.java
+++ PrimeFactorization/src/SieveTest.java
@@ -23,6 +23,11 @@ public class SieveTest {
 		assertEquals(list(2), Sieve.generatePrimes(3));
 	}
 	
+	@Test
+	public void testFour() {
+		assertEquals(list(2, 3), Sieve.generatePrimes(4));
+	}
+	
 	private ArrayList<Integer> list(int... ints){
 		ArrayList<Integer> ret = new ArrayList<Integer>();
 		for(int i : ints){
